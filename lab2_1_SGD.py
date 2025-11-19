308d1d5e2ad6497c0a7b7d19e71acfa3594a3ebd
olegsgit <osipaualeh@gmail.com>
2025-11-19 23:02:56 +0300

    96+% model is saved

---
+++ lab2_1_SGD.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Загружаем готовый файл
+data = np.load("data/processed/notmnist.npz")
+
+x_train, y_train = data["x_train"], data["y_train"]
+x_valid, y_valid = data["x_valid"], data["y_valid"]
+x_test,  y_test  = data["x_test"],  data["y_test"]
+
+print("train:", x_train.shape, y_train.shape)
+print("valid:", x_valid.shape, y_valid.shape)
+print("test:",  x_test.shape,  y_test.shape)
+
+model = models.Sequential([
+    layers.Input(shape=(784,)),                 # вход: 784 пикселя
+    layers.Dense(512, activation='relu'),       # скрытый слой 1: 512 нейронов и relu активация(кусочно-линейная)   
+    layers.Dense(256, activation='relu'),       # скрытый слой 2: 256 нейронов
+    layers.Dense(128, activation='relu'),       # скрытый слой 3: 128 нейронов
+    layers.Dense(10, activation='softmax')      # выход: 10 классов
+])
+
+opt = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+
+model.compile(optimizer=opt,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(
+    x_train, y_train,
+    validation_data=(x_valid, y_valid),
+    epochs=100,            
+    batch_size=256,
+    verbose=2
+)
+
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+print("Точность на тесте:", test_acc)
+
+# Сохраняем модель в формате HDF5 (.h5)
+model.save("models/notmnist_mlp2.h5")
